4c179350d34bef785bfa3e6675653fbbe18185a9
Mohamed talaat <motalaat1610@gmail.com>
2025-01-04 16:34:55 +0200

    demo added

---
+++ lib/googleMapTest.dart
@@ -0,0 +1,37 @@
+import 'package:e_commerce_app/core/functions/ckeckInternet.dart';
+import 'package:flutter/material.dart';
+
+class Test extends StatefulWidget {
+  Test({Key? key}) : super(key: key);
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  ckeck() async {
+    var res = await checkTheInternet();
+    debugPrint(res);
+  }
+
+  @override
+  void initState() {
+    ckeck();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Test'),
+      ),
+      body: Container(
+          padding: EdgeInsets.all(20),
+          child: ListView(
+            children: [],
+          )),
+    );
+  }
+}
